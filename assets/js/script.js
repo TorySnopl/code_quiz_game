1517e76ee299c92d4008b218340f8177d820c9f9
TorySnopl <Paramedic064@gmail.com>
2023-08-11 21:54:47 -0700

    building game function, question property pfills

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,19 +2,63 @@ let startBtn = document.querySelector('.startbtn');
 let startScreen = document.querySelector('.title');
 let timerEl = document.querySelector('.timer');
 let scoreEl = document.querySelector('.score');
+let quizBoard = document.querySelector('.quizboard');
+let question = document.querySelector('.question');
+let answer = document.querySelector('.choice');
 
 
-startBtn.addEventListener('click', function(){
-    startBtn.classList.add('hide');
-    startScreen.classList.add('hide');
-})
+timerEl.classList.add('hide');
+scoreEl.classList.add('hide');
+quizBoard.classList.add('hide');
+
+
+startBtn.addEventListener('click', quizStart);
 
 const questions = [
-    {ask: 'What does Var stand for?', a: 'Variable', w: 'very difficult', w: 'varied', w: 'victory'},
-    {ask: 'what tag in HTML looks at your javascript?', a: 'script', w: 'java', w: 'js', w: 'link'},
-    {ask: 'what symbol will you need to use to get an element by ID?', a: '#', w: '!', w: '%', w: '*'},
-    {ask: 'What is a boolean?', a: 'true/false', w: 'if/else', w: 'and/or', w:'when/where'},
-    {ask: 'What is the correct syntax for a function?', a: 'function name()', w:'function = name()', w:'function: name()', w: 'create.function.name()'},
-    {ask: 'Define i <= 10', a: 'i is less than or equal to 10', w: 'i is always less than 10', w:'is i is less than 10 make it equal to 10', w: 'i isnt actually less than 10' }
+    {ask: 'What does Var stand for?', 
+    a: ['Variable', 'very difficult', 'varied', 'victory',],
+    c: 1},
+
+    {ask: 'what tag in HTML looks at your javascript?', 
+    a: ['script', 'java', 'js', 'link'],
+    c: 1},
+
+    {ask: 'what symbol will you need to use to get an element by ID?', a: ['#',  '!',  '%',  '*'],
+    c: 1},
+
+    {ask: 'What is a boolean?', 
+    a: ['true/false',  'if/else',  'and/or', 'when/where'],
+    c: 1},
+
+    {ask: 'What is the correct syntax for a function?', 
+    a: ['function name()','function = name()','function: name()','create.function.name()'],
+    c:1},
+
+    {ask: 'Define i <= 10', 
+    a: ['i is less than or equal to 10',  'i is always less than 10', 'is i is less than 10 make it equal to 10',  'i isnt actually less than 10' ],
+    c:1}
 ];
 
+function quizStart(){
+    startBtn.classList.add('hide');
+    startScreen.classList.add('hide');
+    timerEl.classList.remove('hide');
+    scoreEl.classList.remove('hide');
+    quizBoard.classList.remove('hide');
+
+    let seconds = 20;
+    let timerInterval = setInterval(() => {
+        seconds--;
+        timerEl.textContent = seconds;
+
+        if (seconds === 0){
+            clearInterval(timerInterval);
+        }
+    }, 1000);
+
+    let randomAsk = (questions[Math.floor(Math.random() * questions.length)]);
+
+    question.textContent = randomAsk.ask;
+    
+  
+   }
