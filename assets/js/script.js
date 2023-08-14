15e7ea832ff6656cb3db806ae7ef76f0eca24770
TorySnopl <Paramedic064@gmail.com>
2023-08-13 18:58:48 -0700

    working options and score counter with score being stored to local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,7 @@ let scoreEl = document.querySelector('.score');
 let quizBoard = document.querySelector('.quizboard');
 let question = document.querySelector('.question');
 let answer = document.querySelector('.choice');
-
+let score = 0;
 
 
 timerEl.classList.add('hide');
@@ -15,32 +15,43 @@ quizBoard.classList.add('hide');
 
 startBtn.addEventListener('click', quizStart);
 
+
+
+
 const questions = [
     {ask: 'What does Var stand for?', 
     a: ['Variable', 'very difficult', 'varied', 'victory',],
-    c: 1},
+    c: 0},
 
     {ask: 'what tag in HTML looks at your javascript?', 
     a: ['link', 'java', 'js', 'script'],
-    c: 4},
+    c: 3},
 
     {ask: 'what symbol will you need to use to get an element by ID?', a: ['!',  '#',  '%',  '*'],
-    c: 2},
+    c: 1},
 
     {ask: 'What is a boolean?', 
     a: ['and/or',  'if/else',  'true/false', 'when/where'],
-    c: 3},
+    c: 2},
 
     {ask: 'What is the correct syntax for a function?', 
     a: ['function name()','function = name()','function: name()','create.function.name()'],
-    c:1},
+    c:0},
 
     {ask: 'Define i <= 10', 
     a: ['i is always less than 10',  'i is less than or equal to 10', 'is i is less than 10 make it equal to 10',  'i isnt actually less than 10' ],
-    c:2}
+    c:1}
 ];
+let randomAsk = (questions[Math.floor(Math.random() * questions.length)]);
+
+    question.textContent = randomAsk.ask;
+
+  
+
+
 
 function quizStart(){
+
     startBtn.classList.add('hide');
     startScreen.classList.add('hide');
     timerEl.classList.remove('hide');
@@ -57,13 +68,7 @@ function quizStart(){
         }
     }, 1000);
 
-};
-
-    
-let randomAsk = (questions[Math.floor(Math.random() * questions.length)]);
-
-    question.textContent = randomAsk.ask;
-
+  
     let userChoice = randomAsk.a;
 
     userChoice.forEach((choice) => {
@@ -73,14 +78,22 @@ let randomAsk = (questions[Math.floor(Math.random() * questions.length)]);
 
         
             options.addEventListener('click', function(){
-                if (options==randomAsk.c){
-                    scoreEl++;
+                if (options.innerHTML==randomAsk.a[randomAsk.c]){
+                    score++;
+                    scoreEl.textContent = score;
+                    localStorage.setItem('score', score);
+                   
+                 } else {
+                    seconds--;
+                    
+                
+                    
+                    
                 }
             })
     });
 
- 
-
+};
 
     
   
