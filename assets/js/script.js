daf8b18b9255712593d59f8a0adc7d3c30749c43
TorySnopl <Paramedic064@gmail.com>
2023-08-13 19:38:15 -0700

    new function for choosing questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,12 +5,16 @@ let scoreEl = document.querySelector('.score');
 let quizBoard = document.querySelector('.quizboard');
 let question = document.querySelector('.question');
 let answer = document.querySelector('.choice');
+let feedback = document.querySelector('.result');
+let finalScreen =document.querySelector('.finalscreen');
 let score = 0;
+let questionCount = 0;
 
 
 timerEl.classList.add('hide');
 scoreEl.classList.add('hide');
 quizBoard.classList.add('hide');
+finalScreen.classList.add('hide');
 
 
 startBtn.addEventListener('click', quizStart);
@@ -42,15 +46,42 @@ const questions = [
     a: ['i is always less than 10',  'i is less than or equal to 10', 'is i is less than 10 make it equal to 10',  'i isnt actually less than 10' ],
     c:1}
 ];
-let randomAsk = (questions[Math.floor(Math.random() * questions.length)]);
-
-    question.textContent = randomAsk.ask;
-
-  
 
 
+function newQuestion(){
+
+    question.textContent = questions[questionCount].ask;
+
+    let userChoice = questions[questionCount].a;
+
+    questionCount++;
+
+    userChoice.forEach((choice) => {
+            let options = document.createElement('p');
+            options.textContent = choice;
+            answer.append(options);
+
+        
+            options.addEventListener('click', function(){
+                if (options.innerHTML==questions[questionCount].c){
+                    score++;
+                    scoreEl.textContent = score;
+                    feedback.textContent = "Way To Go!"
+                    localStorage.setItem('score', score);
+                   
+                 } else {
+                    seconds--;
+                    feedback.textContent = "Not Quite!"
+                 }
+            newQuestion();
+            })
+            
+    });
+
+};
 
 function quizStart(){
+
 
     startBtn.classList.add('hide');
     startScreen.classList.add('hide');
@@ -69,29 +100,7 @@ function quizStart(){
     }, 1000);
 
   
-    let userChoice = randomAsk.a;
 
-    userChoice.forEach((choice) => {
-            let options = document.createElement('p');
-            options.textContent = choice;
-            answer.append(options);
-
-        
-            options.addEventListener('click', function(){
-                if (options.innerHTML==randomAsk.a[randomAsk.c]){
-                    score++;
-                    scoreEl.textContent = score;
-                    localStorage.setItem('score', score);
-                   
-                 } else {
-                    seconds--;
-                    
-                
-                    
-                    
-                }
-            })
-    });
 
 };
 
